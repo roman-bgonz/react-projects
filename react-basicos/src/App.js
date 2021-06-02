ab7e45884f74e9b116bb2389909ac664c708a42b
Roman Badillo Gonzalez <rbadillo@elektra.com.mx>
2021-06-02 09:26:31 -0500

    Uso de estados

--- react-basicos/src/App.js
+++ react-basicos/src/App.js
@@ -3,25 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Componente from "./components/Componente";
 import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> y save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
+        <hr class="hr-text" data-content="Componente" />
         <Componente msg="Hola soy un componente desde las props" />
+
+        <hr class="hr-text" data-content="Propiedades" />
         <Propiedades
           cadena="Es una cadena de texto"
           numero={19}
@@ -34,6 +27,9 @@ function App() {
             <Componente msg="Soy un componente pasado como props" />
           }
         />
+
+        <hr class="hr-text" data-content="Estado" />
+        <Estado />
       </header>
     </div>
   );
---
+++ react-basicos/src/components/Estado.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+function EstadoHijo(props) {
+  return (
+    <div>
+      <h3>{props.contadorHijo}</h3>
+    </div>
+  );
+}
+
+export default class Estado extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+    };
+
+    setInterval(() => {
+      this.setState({
+        contador: this.state.contador + 1,
+      });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div>
+        El estate
+        <p>{this.state.contador}</p>
+        <EstadoHijo contadorHijo={this.state.contador} />
+      </div>
+    );
+  }
+}
