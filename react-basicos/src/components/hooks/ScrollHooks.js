1922736366d13f3edf43e0a596c0e4247c83a0a8
Roman Badillo Gonzalez <rbadillo@elektra.com.mx>
2021-06-08 17:09:21 -0500

    Use effect

--- react-basicos/src/components/hooks/ScrollHooks.js
+++ react-basicos/src/components/hooks/ScrollHooks.js
@@ -4,8 +4,33 @@ export default function ScrollHooks() {
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
+    console.log("Moviendo el scroll");
+
+    const detectarScroll = () => setScrollY(window.pageYOffset);
+
+    // To the event listener we attach the function
+    window.addEventListener("scroll", detectarScroll);
+
+    return () => {
+      console.log("Fase de desmontaje");
+      window.removeEventListener("scroll", detectarScroll);
+    };
+  }, [scrollY]); //Solo se actualiza cuando la variable sufra cambios
+
+  useEffect(() => {
     console.log("Fase de montaje");
   }, []);
+
+  useEffect(() => {
+    console.log("Fase de actualización");
+  });
+
+  useEffect(() => {
+    return () => {
+      console.log("Fase de desmontaje");
+    };
+  });
+
   return (
     <>
       <h2>Hooks - useEffect y el ciclo de vida</h2>
