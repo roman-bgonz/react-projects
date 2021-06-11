d3a0837134128c05bfd5c1f038d4a2f23f68ccd0
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-06-11 13:15:15 -0500

    Creación de elementos base del crud, formulario, dummy data, tabla

--- react-ejercicios/src/App.js
+++ react-ejercicios/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import CrudApp from './components/CrudApp';
+// Curso https://www.youtube.com/watch?v=b-PcWvzf9Xs&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=28
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Ejercicios con React</h1>
+      <CrudApp />
+    </>
   );
 }
 
---
+++ react-ejercicios/src/components/CrudApp.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+const initialDb = [
+  {
+    id: 1,
+    name: 'Seiya',
+    constellation: 'Pegaso',
+  },
+  {
+    id: 2,
+    name: 'Shiryu',
+    constellation: 'Dragón',
+  },
+  {
+    id: 3,
+    name: 'Hyoga',
+    constellation: 'Cisne',
+  },
+  {
+    id: 4,
+    name: 'Shun',
+    constellation: 'Andrómeda',
+  },
+  {
+    id: 5,
+    name: 'Ikki',
+    constellation: 'Fénix',
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+
+  return (
+    <div>
+      CRUD App
+      <CrudForm />
+      <CrudTable data={db} />
+    </div>
+  );
+};
+
+export default CrudApp;
