6eee2c1f48e0d7dd1c3937c89f718ade5dd2debe
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-06-11 11:56:07 -0500

    Instalación de react bootstrap y material ui

--- react-frameworks-css/src/App.js
+++ react-frameworks-css/src/App.js
@@ -1,13 +1,18 @@
 import Bootstrap from './components/Bootstrap';
 import Bulma from './components/Bulma';
+import MaterialUI from './components/MaterialUI';
+import ReactBootstrap from './components/ReactBootstrap';
 
 function App() {
   return (
     <div>
       <h1>Framewoks CSS con React</h1>
-      <Bootstrap />
+      {/* <Bootstrap />
       <br />
       <Bulma />
+      <br />
+      <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
---
+++ react-frameworks-css/src/components/MaterialUI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MaterialCard from './MaterialCard';
+import MaterialDrawer from './MaterialDrawer';
+
+export default function MaterialUI() {
+  return (
+    <>
+      <h2>Material UI</h2>
+      <MaterialCard />
+      <br />
+      <MaterialDrawer />
+    </>
+  );
+}
