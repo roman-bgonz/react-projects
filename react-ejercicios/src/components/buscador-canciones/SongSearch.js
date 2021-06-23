441cdb634681e5c1a4ab225398dda795769521d6
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-06-23 18:44:34 -0500

    Se consumen apis de artista y liric

--- react-ejercicios/src/components/buscador-canciones/SongSearch.js
+++ react-ejercicios/src/components/buscador-canciones/SongSearch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { helpHttp } from '../../helpers/helpHttp';
 import Loader from '../Loader';
 import SongDetails from './SongDetails';
 import SongForm from './SongForm';
@@ -9,8 +10,31 @@ const SongSearch = () => {
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+      let artistUrl = `https://theaudiodb.com/api/v1/json/search.php?s=${artist}`;
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+      setLoading(true);
+
+      // Ponemos await para obtener la información y para que las variables de estado no truene con undefined
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(songUrl),
+      ]);
+
+      setBio(artistRes);
+      setLyric(songRes);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [search]);
+
   const handleSearch = (data) => {
-    console.log(data);
+    //console.log(data);
     setSearch(data);
   };
 
