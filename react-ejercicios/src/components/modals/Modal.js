a8da2cd611cf105122ea90fa363e42a6be060bfc
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-06-25 17:20:58 -0500

    Adición de lógica y css para abrir y cerrar las modales creadas

--- react-ejercicios/src/components/modals/Modal.js
+++ react-ejercicios/src/components/modals/Modal.js
@@ -3,11 +3,15 @@ import './Modal.css';
 /*
  * La propiedad children hace referencia al contendo que se le ponga en modal, ejemplo <Modal>Este es el children</Modal>
  */
-const Modal = ({ children }) => {
+const Modal = ({ children, isOpen, closeModal }) => {
+  //Evitamos que al dar click dentro de nuestro article, se cierre la modal
+  const handleModalContainerClick = (e) => e.stopPropagation();
   return (
-    <article className="modal-is-open">
-      <div className="modal-container">
-        <button className="modal-close">X</button>
+    <article className={`modal ${isOpen && 'is-open'}`} onClick={closeModal}>
+      <div className="modal-container" onClick={handleModalContainerClick}>
+        <button className="modal-close" onClick={closeModal}>
+          X
+        </button>
         {children}
       </div>
     </article>
--- react-ejercicios/src/components/modals/Modals.js
+++ react-ejercicios/src/components/modals/Modals.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useModal } from '../hooks/useModal';
 import Modal from './Modal';
 
 const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
   return (
     <div>
       <h2>Modales</h2>
-      <button>Modal 1</button>
-      <Modal>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3>Modal 1</h3>
         <p>Hola, este es el contenido de la modal 1</p>
         <img src="https://placeimg.com/400/400/arch" alt="Architecture" />
+      </Modal>
+
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+        <h3>Otor modal</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          similique dolorum, fugit dolores laboriosam porro voluptatibus
+          excepturi rerum aliquid odit corporis necessitatibus quo numquam quos.
+          Quo dolor optio facere quis.
+        </p>
+        <img src="https://placeimg.com/400/400/nature" alt="Nature" />
       </Modal>
     </div>
   );
