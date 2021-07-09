f4c5c955db73426e9bdc110ddc5c3485fb268792
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-07-09 13:18:02 -0500

    Proyecto base para api context

---
+++ react-context/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyPage from './components/MyPage';
+
+function App() {
+  return (
+    <div>
+      <h1>React Context API</h1>
+      <MyPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-context/src/components/MyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyPage = () => {
+  return (
+    <div>
+      <h2>Mi página</h2>
+    </div>
+  );
+};
+
+export default MyPage;
