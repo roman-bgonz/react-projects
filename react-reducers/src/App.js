2daa6545a4b2a37a3b94c96761eaca81a859d9f1
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-07-12 18:47:50 -0500

    Creación de archivos base de reducers y estrcutura del componente de carrito

--- react-reducers/src/App.js
+++ react-reducers/src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
 import Contador from './components/Contador';
 import ContadorMejorado from './components/ContadorMejorado';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>
-        <ul>
-          - Reducer es muy parecido a Redux pero esta ya viene incluida en el
-          proyecto = Reducer:
-          <li>Función reductora</li>
-          <li>
-            Devuelve un valor y todo la lógica de la función es capaz de
-            resolver un solo procesamiento sin afectar elementos externos
-          </li>
-          <li>
-            Siempre retornan un valor que se considera el estdo de la aplicación
-          </li>
-          <li>No podemos usar useEffect dentro de funciones reductoras</li>
-          <li>No podemos realizar peticiones adíncronas.</li>
-          <li>Usarlo cuando la app ya tiene una mediana complejidad</li>
-        </ul>
-        <hr />
-        <ContadorMejorado />
-        <hr />
-        <Contador />
-      </h1>
+      <ul>
+        - Reducer es muy parecido a Redux pero esta ya viene incluida en el
+        proyecto = Reducer:
+        <li>Función reductora</li>
+        <li>
+          Devuelve un valor y todo la lógica de la función es capaz de resolver
+          un solo procesamiento sin afectar elementos externos
+        </li>
+        <li>
+          Siempre retornan un valor que se considera el estdo de la aplicación
+        </li>
+        <li>No podemos usar useEffect dentro de funciones reductoras</li>
+        <li>No podemos realizar peticiones adíncronas.</li>
+        <li>Usarlo cuando la app ya tiene una mediana complejidad</li>
+      </ul>
+      <hr />
+      <ShoppingCart />
+      <hr />
+      <ContadorMejorado />
+      <hr />
+      <Contador />
     </div>
   );
 }
---
+++ react-reducers/src/reducers/shoppingReducer.js
@@ -0,0 +1,55 @@
+import { TYPES } from '../actions/shoppingActions';
+
+export const shoppingInitialState = {
+  products: [
+    {
+      id: 1,
+      name: 'Producto 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      price: 200,
+    },
+    {
+      id: 3,
+      name: 'Producto 3',
+      price: 300,
+    },
+    {
+      id: 4,
+      name: 'Producto 4',
+      price: 400,
+    },
+    {
+      id: 5,
+      name: 'Producto 5',
+      price: 500,
+    },
+  ],
+  cart: [],
+};
+
+export function shoppingReducer(state, action) {
+  switch (action.type) {
+    case TYPES.ADD_TO_CART:
+      {
+      }
+      break;
+    case TYPES.REMOVE_ONE_FROM_CART:
+      {
+      }
+      break;
+    case TYPES.REMOVE_ALL_FROM_CART:
+      {
+      }
+      break;
+    case TYPES.CLEAR_CART:
+      {
+      }
+      break;
+    default:
+      return state;
+  }
+}
