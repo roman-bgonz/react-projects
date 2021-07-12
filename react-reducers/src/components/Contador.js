75521fb03d30ea66b5c92c55f36d80a2c4dbf2d7
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-07-12 17:14:11 -0500

    Se actualiza el contador pero con reducer

--- react-reducers/src/components/Contador.js
+++ react-reducers/src/components/Contador.js
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+const initialState = { contador: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { contador: state.contador + 1 };
+    case 'DECREMENT':
+      return { contador: state.contador - 1 };
+    default:
+      return state;
+  }
+}
 
 const Contador = () => {
-  const [contador, setCount] = useState(0);
-  const sumar = () => {
-    setCount(contador + 1);
-  };
-  const restar = () => {
-    setCount(contador - 1);
-  };
+  //const [contador, setCount] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const sumar = () => dispatch({ type: 'INCREMENT' });
+  const restar = () => dispatch({ type: 'DECREMENT' });
   return (
     <div>
       <h2>Contador Reducer</h2>
@@ -15,7 +26,7 @@ const Contador = () => {
         <button onClick={sumar}>+</button>
         <button onClick={restar}>-</button>
       </nav>
-      <h3>{contador}</h3>
+      <h3>{state.contador}</h3>
     </div>
   );
 };
