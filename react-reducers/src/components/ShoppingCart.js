e44813cb8b43f994dcdf14db03dcf5479a958090
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2021-07-20 09:56:49 -0500

    Se agregan estilos y lista de productos

--- react-reducers/src/components/ShoppingCart.js
+++ react-reducers/src/components/ShoppingCart.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import {
+  shoppingInitialState,
+  shoppingReducer,
+} from '../reducers/shoppingReducer';
+import ProductItem from './ProductItem';
 
 const ShoppingCart = () => {
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState);
+  const { products, cart } = state;
+
+  const addToCart = () => {};
+
+  const delFromCart = () => {};
+  const clearCart = () => {};
+
   return (
     <div>
       <h2>Carrito de compras</h2>
       <h3>Productos</h3>
-      <article className="box"></article>
+      <article className="box grid-responsive">
+        {products.map((product) => (
+          <ProductItem key={product.id} data={product} addToCart={addToCart} />
+        ))}
+      </article>
       <h3>Carrito</h3>
       <article className="box"></article>
     </div>
